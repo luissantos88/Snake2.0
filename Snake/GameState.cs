03e66b0d6fec068cc2e08fcc8199e2d51ae54673
dmsan <dmsan@FRESH_DEVELOPER>
2023-09-16 14:57:11 +0100

    Add project files.

---
+++ Snake/GameState.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public class GameState
+    {
+        public int Rows { get; }
+        public int Cols { get; }
+        public GridValue[,] Grid { get; }
+        public Direction Dir { get; private set; }
+        public int Score { get; private set; }
+        public bool GameOver { get; private set; }
+
+        private readonly LinkedList<Direction> dirChanges = new LinkedList<Direction>();
+        private readonly LinkedList<Position> snakePositions = new LinkedList<Position>();
+        private readonly Random random = new Random();
+        volatile bool createFood = false;
+        volatile bool goodFood = true;
+        volatile bool badFoodBool = true;
+        volatile bool badFoodEaten = false;
+        volatile bool drunkFoodBool = true;
+        volatile bool drunkFoodEaten = false;
+        volatile public static bool oppositeDirection = false;
+
+        public GameState(int rows, int cols)
+        {
+            Rows = rows;
+            Cols = cols;
+            Grid = new GridValue[rows, cols];
+            Dir = Direction.Right;
+            AddSnake();
+            AddFood();
+        }
+
+        private void AddSnake()
+        {
+            int r = Rows / 2; // create snake middle of the grid
+
+            for (int c = 1; c <= 3; c++)
+            {
+                Grid[r, c] = GridValue.Snake;
+                snakePositions.AddFirst(new Position(r, c));
+            }
+        }
+
+        private IEnumerable<Position> EmptyPositions() // returns all empty positions on the grid
+        {
+            for (int r = 0; r < Rows; r++)
+            {
+                for (int c = 0; c < Cols; c++)
+                {
+                    if (Grid[r, c] == GridValue.Empty)
+                    {
+                        yield return new Position(r, c);
+                    }
+                }
+            }
+        }
+
+        private void AddFood() // creates random food
+        {
+            List<Position> empty = new List<Position>(EmptyPositions());
+
+            if (empty.Count == 0) // if someone wins the game (no more empty postions)
+            {
+                return;
+            }
+
+            Position pos = empty[random.Next(empty.Count)];
+
+            Random randomNumber = new Random();
+            int num = randomNumber.Next(1, 9);
+
+            while (!createFood && goodFood && badFoodBool && drunkFoodBool)
+            {
+                oppositeDirection = false;
+                drunkFoodEaten = true;
+                createFood = true;
+                badFoodBool = false;
+                drunkFoodBool = false;
+
+                switch (num)
+                {
+                    case <= 6:
+                        Grid[pos.Row, pos.Col] = GridValue.Food;
+                        break;
+                    case 7:
+                        Grid[pos.Row, pos.Col] = GridValue.BadFood;
+                        Task badFoodTask = badFoodtimerAsync(pos);
+                        break;
+                    case 8:
+                        Grid[pos.Row, pos.Col] = GridValue.DrunkFood;
+                        Task drunkFoodTask = oppositeMoveTimer(pos);
+                        break;
+                }
+            }
+        }
+
+        public LinkedList<Position> GetSnakePositions() // returns snake positions
+        {
+            return snakePositions;
+        }
+
+        public Position HeadPosition(LinkedList<Position> snakePositions) // returns the snake head postion
+        {
+            return snakePositions.First.Value;
+        }
+
+        public Position TailPositon() // returns the tail of the snake
+        {
+            if (snakePositions == null)
+            {
+            }
+            return snakePositions.Last.Value;
+        }
+
+        public IEnumerable<Position> SnakePositons()
+        {
+            return snakePositions;
+        }
+
+        private void AddHead(Position pos) // give new position to head of the snake
+        {
+            snakePositions.AddFirst(pos);
+            Grid[pos.Row, pos.Col] = GridValue.Snake;
+        }
+        private void RemoveTail() // remove the tail from the last position
+        {
+            Position tail = snakePositions.Last.Value;
+            Grid[tail.Row, tail.Col] = GridValue.Empty;
+            snakePositions.RemoveLast();
+        }
+
+        private Direction GetLastDirection() // gets snake last direction
+        {
+            if (dirChanges.Count == 0)
+            {
+                return Dir;
+            }
+
+            return dirChanges.Last.Value;
+        }
+        private bool CanChangeDirection(Direction newDir) // checks if snake can change direction
+        {
+            if (dirChanges.Count == 2)
+            {
+                return false;
+            }
+
+            Direction lastDir = GetLastDirection();
+            return newDir != lastDir && newDir != lastDir.Opposite();
+        }
+
+        public void ChangeDirection(Direction dir) // change snake direction
+        {
+            if (CanChangeDirection(dir))
+            {
+                dirChanges.AddLast(dir);
+            }
+        }
+        private bool OutsideGrid(Position pos) // bool to check grid limits
+        {
+            return pos.Row < 0 || pos.Row >= Rows || pos.Col < 0 || pos.Col >= Cols;
+        }
+
+        private GridValue WillHit(Position newHeadPos) // check hit situations of snake
+        {
+            if (OutsideGrid(newHeadPos)) // if snake hit grid limits
+            {
+                return GridValue.Outside;
+            }
+
+            if (newHeadPos == TailPositon()) // if next position of head is tail then snake moves without hit
+            {
+                return GridValue.Empty;
+            }
+
+            return Grid[newHeadPos.Row, newHeadPos.Col];
+        }
+
+        public async Task MoveAsync() // move snake
+        {
+            if (dirChanges.Count > 0)
+            {
+                Dir = dirChanges.First.Value;
+                dirChanges.RemoveFirst();
+            }
+
+            Position newHeadPos = HeadPosition(GetSnakePositions()).Translate(Dir);
+            GridValue hit = WillHit(newHeadPos);
+
+            if (hit == GridValue.Outside || hit == GridValue.Snake)  // if snake moves to grid limts or snake body GAME OVER           
+            {
+                MainWindow.StopPlayMusic();
+                GameOver = true;
+                MainWindow.PlaySnakeDead();
+            }
+            else if (hit == GridValue.Empty) // if snake moves to empty postision do nothing just moves
+            {
+                RemoveTail();
+                AddHead(newHeadPos);
+            }
+            else if (hit == GridValue.Food) // if snake moves to food generates another random food
+            {
+                goodFood = true;
+                badFoodBool = true;
+                drunkFoodBool = true;
+                Score++;
+                AddHead(newHeadPos);
+                createFood = false;
+                AddFood();
+            }
+            else if (hit == GridValue.BadFood) // if snake moves to badFood score 1- and losses 1 bodypart
+            {
+                Score--;
+                badFoodEaten = true;
+                drunkFoodEaten = true;
+                goodFood = true;
+                drunkFoodBool = true;
+                badFoodBool = true;
+                RemoveTail();
+                RemoveTail();
+                AddHead(newHeadPos);
+                createFood = false;
+                AddFood();
+            }
+            else if (hit == GridValue.DrunkFood) // if snake moves to drunkfood score 2+ and direction is inverted for 5 seconds
+            {
+                Score += 2;
+                drunkFoodEaten = true;
+                oppositeDirection = true;
+                createFood = true;
+                badFoodBool = false;
+                drunkFoodBool = false;
+                goodFood = false;
+
+                AddHead(newHeadPos);
+
+                int timer = 0;
+
+                while (timer < 5)
+                {
+                    timer++;
+                    await Task.Delay(1000);
+                }
+
+                badFoodBool = true;
+                badFoodEaten = false;
+                drunkFoodBool = true;
+                goodFood = true;
+                drunkFoodEaten = false;
+                oppositeDirection = false;
+                createFood = false;
+                AddFood();
+            }
+        }
+        public async Task badFoodtimerAsync(Position pos) // creates a 5 seconds timer for the badfood 
+        {
+            badFoodEaten = false;
+            createFood = true;
+            goodFood = false;
+            badFoodBool = false;
+            drunkFoodBool = false;
+            Position newHeadPos = HeadPosition(GetSnakePositions()).Translate(Dir);
+            GridValue hit = WillHit(newHeadPos);
+            int timer = 0;
+
+            while (timer < 5)
+            {
+                timer++;
+                await Task.Delay(1000);
+            }
+
+            if (!badFoodEaten)
+            {
+                Grid[pos.Row, pos.Col] = GridValue.Empty;
+                createFood = false;
+                goodFood = true;
+                badFoodBool = true;
+                drunkFoodBool = true;
+                badFoodEaten = true;
+                AddFood();
+            }
+        }
+        public async Task oppositeMoveTimer(Position pos) // creates a 5 seconds timer for the drunkFood
+        {
+            drunkFoodEaten = false;
+            goodFood = false;
+            drunkFoodBool = false;
+            badFoodBool = false;
+            Position newHeadPos = HeadPosition(GetSnakePositions()).Translate(Dir);
+            GridValue hit = WillHit(newHeadPos);
+            createFood = true;
+            int timer = 0;
+
+            while (timer < 5)
+            {
+                timer++;
+                await Task.Delay(1000);
+            }
+
+            if (!drunkFoodEaten)
+            {
+                Grid[pos.Row, pos.Col] = GridValue.Empty;
+                createFood = false;
+                goodFood = true;
+                drunkFoodBool = true;
+                badFoodBool = true;
+                drunkFoodEaten = true;
+                AddFood();
+            }
+        }
+    }
+}
---
+++ Snake/MainWindow.xaml.cs
@@ -0,0 +1,269 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Media;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+
+namespace Snake
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        private readonly Dictionary<GridValue, ImageSource> gridValToImage = new()
+        {
+            { GridValue.Empty, Images.Empty },
+            { GridValue.Snake, Images.Body},
+            { GridValue.Food, Images.Food},
+            { GridValue.BadFood, Images.BadFood},
+            { GridValue.DrunkFood, Images.DrunkFood},
+        };
+        private readonly Dictionary<Direction, int> dirToRotation = new()
+        {
+            { Direction.Up, 0 },
+            { Direction.Right, 90 },
+            { Direction.Down, 180 },
+            { Direction.Left, 270 }
+        };
+        private readonly int rows = 20, cols = 20;
+        private readonly Image[,] gridImages;
+        private GameState gameState;
+        private bool gameRunning;
+        int score;
+        int highScore;
+
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            ReadHighScore();
+            gridImages = SetupGrid();
+            gameState = new GameState(rows, cols);
+        }
+
+        private async Task RunGame()
+        {
+            GameState.oppositeDirection = false;
+            Draw();
+            await ShowCountDown();
+            Overlay.Visibility = Visibility.Hidden;
+            PlayMusic();
+            await GameLoop();
+            await ShowGameOver();
+            gameState = new GameState(rows, cols);
+
+        }
+
+        private async void Window_PreviewKeyDown(object sender, KeyEventArgs e) // and user press any key this method is called
+        {
+            if (Overlay.Visibility == Visibility.Visible)
+            {
+                e.Handled = true; // prevents Window_KeyDown to be called
+            }
+
+            if (!gameRunning)
+            {
+                gameRunning = true;
+                await RunGame();
+                gameRunning = false;
+            }
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e) // give input to keyboard keys
+        {
+            if (gameState.GameOver)
+            {
+                return;
+            }
+
+            if (GameState.oppositeDirection)
+            {
+                switch (e.Key)
+                {
+                    case Key.Left:
+                        gameState.ChangeDirection(Direction.Right);
+                        break;
+                    case Key.Right:
+                        gameState.ChangeDirection(Direction.Left);
+                        break;
+                    case Key.Up:
+                        gameState.ChangeDirection(Direction.Down);
+                        break;
+                    case Key.Down:
+                        gameState.ChangeDirection(Direction.Up);
+                        break;
+                }
+            }
+
+            if (!GameState.oppositeDirection)
+            {
+                switch (e.Key)
+                {
+                    case Key.Left:
+                        gameState.ChangeDirection(Direction.Left);
+                        break;
+                    case Key.Right:
+                        gameState.ChangeDirection(Direction.Right);
+                        break;
+                    case Key.Up:
+                        gameState.ChangeDirection(Direction.Up);
+                        break;
+                    case Key.Down:
+                        gameState.ChangeDirection(Direction.Down);
+                        break;
+                }
+            }
+        }
+
+        private async Task GameLoop()
+        {
+            while (!gameState.GameOver)
+            {
+                await Task.Delay(100);
+                gameState.MoveAsync();
+                Draw();
+            }
+        }
+
+        private Image[,] SetupGrid()
+        {
+            Image[,] images = new Image[rows, cols];
+            GameGrid.Rows = rows;
+            GameGrid.Columns = cols;
+            GameGrid.Width = GameGrid.Height * (cols / (double)rows);
+
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    Image image = new Image
+                    {
+                        Source = Images.Empty,
+                        RenderTransformOrigin = new Point(0.5, 0.5)
+                    };
+
+                    images[r, c] = image;
+                    GameGrid.Children.Add(image);
+                }
+            }
+            return images;
+        }
+        private void Draw()
+        {
+            DrawGrid();
+            DrawSnakeHead();
+            ScoreAndHighScore();
+        }
+
+        private void ReadHighScore()
+        {
+            highScore = 0; // Initialize highScore to a default value in case the file doesn't exist or doesn't contain a valid integer.
+
+            if (!File.Exists("highScores.txt"))
+            {
+                TextWriter textWriter = new StreamWriter("highScores.txt");
+                textWriter.Write(highScore);
+                textWriter.Close();
+            }
+
+            string line;
+            TextReader textReader = new StreamReader("highScores.txt");
+            line = textReader.ReadLine();
+            highScore = int.Parse(line);
+            textReader.Close();
+            HighScoreText.Text = "HighScore " + highScore;
+        }
+        private void WriteHighScore()
+        {
+
+            if (score > highScore)
+            {
+                highScore = score;
+
+                HighScoreText.Text = "HighScore " + highScore;
+                HighScoreText.Foreground = new SolidColorBrush(Colors.Gold);
+                TextWriter textWriter = new StreamWriter("highScores.txt");
+                textWriter.WriteLine(highScore);
+                textWriter.Close();
+            }
+        }
+        private void ScoreAndHighScore()
+        {
+            score = gameState.Score;
+            ScoreText.Text = $"Score " + score;
+        }
+        private void DrawGrid() // draws the images to every positon of the grid
+        {
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    GridValue gridVal = gameState.Grid[r, c];
+                    gridImages[r, c].Source = gridValToImage[gridVal];
+                    gridImages[r, c].RenderTransform = Transform.Identity; // image rotate is only the head
+                }
+            }
+        }
+        private void DrawSnakeHead()
+        {
+            Position headPos = gameState.HeadPosition(gameState.GetSnakePositions());
+            Image image = gridImages[headPos.Row, headPos.Col];
+            image.Source = Images.Head;
+
+            int rotation = dirToRotation[gameState.Dir];
+            image.RenderTransform = new RotateTransform(rotation);
+        }
+        private async Task DrawDeadSnake()
+        {
+            List<Position> positions = new List<Position>(gameState.SnakePositons());
+
+            for (int i = 0; i < positions.Count; i++)
+            {
+                Position pos = positions[i];
+                ImageSource source = (i == 0) ? Images.DeadHead : Images.DeadBody;
+                gridImages[pos.Row, pos.Col].Source = source;
+                await Task.Delay(50);
+            }
+        }
+        private async Task ShowCountDown()
+        {
+            for (int i = 3; i >= 1; i--)
+            {
+                OverlayText.Text = i.ToString();
+                await Task.Delay(500);
+            }
+        }
+
+        private async Task ShowGameOver()
+        {
+            WriteHighScore();
+            await DrawDeadSnake();
+            await Task.Delay(1000);
+            Overlay.Visibility = Visibility.Visible;
+            OverlayText.Text = "PRESS ANY KEY TO START";
+            HighScoreText.Foreground = new SolidColorBrush(Colors.White);
+        }
+        public static void PlayMusic() // play snake theme music
+        {
+            SoundPlayer musicPlayer = new SoundPlayer(Properties.Resource1.snakeMusic);
+            musicPlayer.PlayLooping();
+        }
+        public static void StopPlayMusic() // stops snake theme music
+        {
+            SoundPlayer musicPlayer = new SoundPlayer(Properties.Resource1.snakeMusic);
+            musicPlayer.Stop();
+        }
+        public static void PlaySnakeDead() // play snake dead sound
+        {
+            SoundPlayer musicPlayer = new SoundPlayer(Properties.Resource1.snakeDead);
+            musicPlayer.Play();
+        }
+    }
+}
